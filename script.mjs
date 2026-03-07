d7891d4b245f03ab9dd3293bbbb0c514c989a187
avatarit <salah@ait.ps>
2026-03-07 11:54:14 +0000

    Add user selection dropdown and populate with user IDs

--- script.mjs
+++ script.mjs
@@ -1,11 +1,15 @@
-// This is a placeholder file which shows how you can access functions defined in other files.
-// It can be loaded into index.html.
-// You can delete the contents of the file once you have understood how it works.
-// Note that when running locally, in order to open a web page which uses modules, you must serve the directory over HTTP e.g. with https://www.npmjs.com/package/http-server
-// You can't open the index.html file using a file:// URL.
+import { getUserIDs } from "./data.mjs";
 
-import { countUsers } from "./common.mjs";
+const select = document.getElementById("user-select");
+const userIds = getUserIDs();
+
 
 window.onload = function () {
-  document.querySelector("body").innerText = `There are ${countUsers()} users`;
+  for (const user of userIds) {
+    const option = document.createElement("option");
+    option.value = user;
+    option.textContent = "User " + user;
+    select.appendChild(option);
+  }
+
 };
